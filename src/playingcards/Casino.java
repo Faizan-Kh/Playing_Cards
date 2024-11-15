2b3ec8b159dd0182642c207bc64366a18482a557
Faizan <faizanswabi95@gmail.com>
2024-11-15 20:38:40 +0500

    Casino class added

---
+++ src/playingcards/Casino.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package playingcards;
+
+/**
+ *
+ * @author Faizan
+ */
+public class Casino {
+    String bartender;
+
+    public Casino(String bartender) {
+        this.bartender = bartender;
+    }
+    
+    
+}
